aa88b23f3f1b72ac0381dc97c3cd12e42a55a8fc
Anjali Kumari <sayhellotoanjali@gmail.com>
2023-08-24 10:19:52 +0530

    Conditional statements.

---
+++ Day3/else2.js
@@ -0,0 +1,16 @@
+//create a traffic light system that shows what to do based on color.
+//use of else
+let color="rd";
+
+//traffic light system
+if(color === "red"){
+    console.log("STOP! LIGHT COLOR IS RED");
+}
+else if(color === "yellow"){
+    console.log("SLOW DOWN, LIGHT COLOR IS YELLOW");
+}
+else if(color === "green"){
+    console.log("GO, LIGHT COLOR IS GREEN");
+} else {
+    console.log("Traffic light is broken");
+}
