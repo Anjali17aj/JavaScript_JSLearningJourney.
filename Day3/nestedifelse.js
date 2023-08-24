2e8baae58962c827ea1c4741c3e5673b642b6aca
Anjali Kumari <sayhellotoanjali@gmail.com>
2023-08-24 10:51:22 +0530

    Conditional statements.

---
+++ Day3/nestedifelse.js
@@ -0,0 +1,13 @@
+let marks = 32;
+
+if(marks>=33){
+    console.log("Pass");
+    if(marks>=70){
+        console.log("Grade : outstanding");
+    } else {
+        console.log("Grade : A");
+    }
+
+} else {
+    console.log("Better luck next time!");
+}
