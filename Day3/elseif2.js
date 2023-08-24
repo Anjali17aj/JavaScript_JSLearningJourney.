37bd8d33b86ec2f58f847b15373b594b8f6a84e0
Anjali Kumari <sayhellotoanjali@gmail.com>
2023-08-24 10:15:07 +0530

    Conditional statements.

---
+++ Day3/elseif2.js
@@ -0,0 +1,16 @@
+let marks = 90; //20,55
+if(marks>=90){
+    console.log("A+");
+}
+else if(marks>=80){
+    console.log("A");
+}
+else if(marks>=70){
+    console.log("B");
+}
+else if(marks>=40){
+    console.log("C");
+}
+else if(marks<33){
+    console.log("fail");
+}
