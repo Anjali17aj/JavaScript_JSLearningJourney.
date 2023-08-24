98a60dd4efe352c651ef2fcb56f0e54373b03b03
Anjali Kumari <sayhellotoanjali@gmail.com>
2023-08-24 10:29:33 +0530

    Conditional statements.

---
+++ Day3/popcornPrice.js
@@ -0,0 +1,17 @@
+let size ="M";
+
+if(size === "XL"){
+console.log("price is Rs. 350");
+}
+ else if(size === "L"){
+    console.log("price is Rs. 300");
+    }
+    else if(size === "M"){
+        console.log("price is Rs. 250");
+        }
+         else if(size === "S"){
+            console.log("price is Rs. 200");
+            }
+            else{
+                console.log("price is Rs. 100");
+                }
