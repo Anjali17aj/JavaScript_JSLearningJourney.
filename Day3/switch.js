c3c9e84974dc29c3a4340745fcf3975e87de1368
Anjali Kumari <sayhellotoanjali@gmail.com>
2023-08-26 08:02:03 +0530

    Switch Statement

---
+++ Day3/switch.js
@@ -0,0 +1,15 @@
+let color="red";
+
+//traffic light system
+switch(color) {
+    case "red":
+    console.log("STOP! LIGHT COLOR IS RED");
+    break;
+     case "yellow":
+    console.log("SLOW DOWN, LIGHT COLOR IS YELLOW");
+    break;
+    case "green":
+    console.log("GO, LIGHT COLOR IS GREEN");
+      default:
+        console.log("The light is broken");
+}
