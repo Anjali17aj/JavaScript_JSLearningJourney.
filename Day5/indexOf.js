acb4808b370a5db0214b26a6fbfd41947bafbd11
Anjali Kumari <sayhellotoanjali@gmail.com>
2023-08-28 12:43:23 +0530

    Arrays

--- Day5/indexOf.js
+++ Day5/indexOf.js
@@ -3,3 +3,5 @@ let cars = ["Audi","Ferrari","Lamborgini","Hyundai","BMW"];
 console.log(cars);
 let idxCars = cars.indexOf("Lamborgini");
 console.log(idxCars);
+let idxof = cars.indexOf("Lambo");
+console.log(idxof); //-1
