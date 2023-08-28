8267da13255888b52f5ea18d671fbb28f5454520
Anjali Kumari <sayhellotoanjali@gmail.com>
2023-08-28 12:47:14 +0530

    Arrays.

---
+++ Day5/include.js
@@ -0,0 +1,7 @@
+//ARRAY indexOf method
+let cars = ["Audi","Ferrari","Lamborgini","Hyundai","BMW"];
+console.log(cars);
+let idxCars = cars.includes("Lamborgini");
+console.log(idxCars);
+let idxof = cars.includes("Lambo");
+console.log(idxof); //false not found
