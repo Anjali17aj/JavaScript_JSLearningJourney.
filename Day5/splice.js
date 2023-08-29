b7b3620472a0eebcf0971202638c9c4d254ffd9e
Anjali Kumari <sayhellotoanjali@gmail.com>
2023-08-29 17:36:55 +0530

    Arrays.

---
+++ Day5/splice.js
@@ -0,0 +1,11 @@
+let cars = ["Audi","xuv","ferrari","porsche","Mustang"];
+console.log(cars);
+console.log(cars.splice());
+console.log(cars.splice(3)); //DELETE 1,2,3
+console.log(cars.splice(1,2));//ONLY PRINT 
+
+
+console.log(cars);
+
+// console.log(cars.splice(cars.length-1));
+// console.log(cars.splice(-2));
---
+++ Day5/splice2.js
@@ -0,0 +1,4 @@
+let cars = ["Audi","xuv","ferrari","porsche","Mustang"];
+console.log(cars);
+cars.splice(0, 0,"toyota","BALENO");
+console.log(cars);
