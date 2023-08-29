29fea2f762c7ef60a0771b9af13e72de05a1028d
Anjali Kumari <sayhellotoanjali@gmail.com>
2023-08-29 21:48:30 +0530

    Arrays.

---
+++ Day5/nestedArr.js
@@ -0,0 +1,5 @@
+let nums = [[9,4],[3,6],[4,8]];
+console.log(nums);
+console.log(nums.length);
+console.log(nums[0].length);
+console.log(nums[0][0]);//print zeroth index's array's first element. *(nums[row][col])
