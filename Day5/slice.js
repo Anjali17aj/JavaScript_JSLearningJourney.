a807b974e24574238839cb633007fef5ce7fa92d
Anjali Kumari <sayhellotoanjali@gmail.com>
2023-08-29 11:18:20 +0530

    Arrays

---
+++ Day5/slice.js
@@ -0,0 +1,7 @@
+let cars = ["Audi","xuv","ferrari","porsche","Mustang"];
+console.log(cars);
+console.log(cars.slice());
+console.log(cars.slice(2));
+console.log(cars.slice(1,3));
+console.log(cars.slice(cars.length-1));
+console.log(cars.slice(-2));
