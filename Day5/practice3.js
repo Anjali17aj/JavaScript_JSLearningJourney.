ba95053496e61d94da307588edae0d2fa83c8d15
Anjali Kumari <sayhellotoanjali@gmail.com>
2023-08-29 20:25:45 +0530

    Arrays.

---
+++ Day5/practice3.js
@@ -0,0 +1,5 @@
+let lang = ["C","C++","JAVA","JAVASCRIPT","PYTHON","C#"];
+lang.push("SQL");
+console.log(lang);
+console.log(lang.reverse());//reverse
+console.log(lang.reverse().indexOf("JAVASCRIPT"));
