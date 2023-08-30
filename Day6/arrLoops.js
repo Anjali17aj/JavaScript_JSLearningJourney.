703ec523ff7cba62f398ed6710d9aa425c2ad369
Anjali Kumari <sayhellotoanjali@gmail.com>
2023-08-30 13:08:51 +0530

    Loops.

---
+++ Day6/arrLoops.js
@@ -0,0 +1,5 @@
+let fruits = ["Mango","Apple","Banana","Litchi","Orange"];
+fruits.push("pineapple");
+for(let i=0; i<fruits.length; i++){
+    console.log(i, fruits[i]);
+}
---
+++ Day6/arrloopsbck.js
@@ -0,0 +1,5 @@
+let fruits = ["Mango","Apple","Banana","Litchi","Orange"];
+fruits.push("pineapple");
+for(let i=fruits.length-1; i>=0; i--){
+    console.log(i, fruits[i]);
+}
