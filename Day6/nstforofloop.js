5656e2b536f939beda62a5339da7ee652db1e785
Anjali Kumari <sayhellotoanjali@gmail.com>
2023-08-31 17:06:48 +0530

    Loops.

---
+++ Day6/nstforofloop.js
@@ -0,0 +1,7 @@
+let heroes = [["ironman","spiderman","thor"],["Captain America","wonder woman","flash"]];
+for(list of heroes){
+    console.log(list);
+    for(hero of list){
+        console.log(hero);
+    }
+}
