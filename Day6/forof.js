e4d4ef7d71bb98ba8a9165b6ce22baa753f8322f
Anjali Kumari <sayhellotoanjali@gmail.com>
2023-08-31 17:01:29 +0530

    Loops.

---
+++ Day6/forof.js
@@ -0,0 +1,8 @@
+let fruits = ["mango","apple","banana","orange","litchi"];
+for(fruit of fruits){ //fruit is a variable where things in fruits get stored.
+    console.log(fruit);
+}
+
+for(char of "anjalikumari"){
+    console.log(char);
+}
