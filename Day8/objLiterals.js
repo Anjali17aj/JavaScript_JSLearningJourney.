456c886d1dccc3d406bc42e810327de1f5a13b7e
Anjali Kumari <sayhellotoanjali@gmail.com>
2023-09-01 08:08:40 +0530

    Object Literals.

---
+++ Day8/objLiterals.js
@@ -0,0 +1,7 @@
+//how to declare an object literal which is used to store 
+
+const student = {
+    name: "Anjali",
+    age: 23,
+    marks: 95
+};
---
+++ Day8/threadTweet.js
@@ -0,0 +1,9 @@
+const post = {
+    username: "@anjali17aj",
+    content: "This is my #firstPost",
+    likes: 177,
+    reposts: 10,
+    tags: ["@prachik", "@chiky18","@anita305"] //tagged multiple account under a post
+};
+
+console.log(post);
