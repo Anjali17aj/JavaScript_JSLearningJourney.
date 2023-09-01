4bed4425771c5970ab9ff7a3fa0b401952f953d7
Anjali Kumari <sayhellotoanjali@gmail.com>
2023-09-01 22:42:24 +0530

    Object Literals.

---
+++ Day8/randmIntegers.js
@@ -0,0 +1,11 @@
+// let step1 =Math.random();
+// console.log(step1);
+// let step2 = step1 * 10;
+// console.log(step2);
+// let step3 = Math.floor(step2); 
+// console.log(step3);
+// let step4 = Math.floor(Step1 * 10 ) +1;
+// console.log(step4);
+
+let random = Math.floor(Math.random() * 10)+1; //range 1 to 10
+console.log(random);
