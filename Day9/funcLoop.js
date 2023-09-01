2106a7a0d50866100779649cf71a6bdb7a342242
Anjali Kumari <sayhellotoanjali@gmail.com>
2023-09-01 23:44:45 +0530

    Functions

---
+++ Day9/funcLoop.js
@@ -0,0 +1,20 @@
+
+
+    function print1to5(){
+        for(let i=1; i<=5; i++){
+            console.log(i);
+            
+        }
+    }
+    print1to5();
+    
+    function isAdult(){
+   let age = 18;
+   if(age >= 18){
+    console.log("Adult");
+   } else {
+    console.log("Not Adult");
+   }
+    }
+
+    isAdult();
---
+++ Day9/functions.js
@@ -0,0 +1,10 @@
+function hello(){
+console.log("Hello");
+}
+
+hello();
+function printName(){
+console.log("Anjali");
+console.log("She lives in India");
+}
+printName();
