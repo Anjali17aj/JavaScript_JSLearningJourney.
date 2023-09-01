61a795aca8e06be075445215d8658c9afdac029a
Anjali Kumari <sayhellotoanjali@gmail.com>
2023-09-01 17:50:15 +0530

    Object Literals.

---
+++ Day8/arrayofobj.js
@@ -0,0 +1,25 @@
+const classInfo = [
+    {
+        name: "Anjali",
+        grade: "A+",
+        city: "Ranchi"
+    },
+    {
+        name: "Ashwini",
+        grade: "A",
+        city: "JSR"
+    },
+    {
+        name: "Anita",
+        grade: "B",
+        city: "Pune"
+    }
+];
+
+console.log(classInfo);
+console.log(classInfo.length);
+console.log(classInfo[0]);
+console.log(classInfo[1].name);
+console.log(classInfo[1].city = "Kerala");
+console.log(classInfo[1].gender = "Male");
+console.log(classInfo);
