4d04c121b64ae43c17095f242b4f84fd1a6abd67
Anjali Kumari <sayhellotoanjali@gmail.com>
2023-09-01 17:42:37 +0530

    Object Literals.

---
+++ Day8/objofobj.js
@@ -0,0 +1,18 @@
+const info = {
+    anjali: {
+        gender: "F",
+        occupation: "Student"
+    },
+    ashwini: {
+        gender: "M",
+        occupation: "Student"
+    },
+    ajay: {
+        gender: "M",
+        occupation: "Service man"
+    }
+};
+
+console.log(info);
+let details= info.ajay.gender;
+console.log(details);
