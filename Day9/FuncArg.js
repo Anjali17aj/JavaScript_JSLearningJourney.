78c42d4afecbe0d45220a7846436458cb6589e28
Anjali Kumari <sayhellotoanjali@gmail.com>
2023-09-02 07:46:22 +0530

    Functions

---
+++ Day9/FuncArg.js
@@ -0,0 +1,4 @@
+function printName(name){
+    console.log(name);
+}
+printName("Anjali");
---
+++ Day9/funcArg2.js
@@ -0,0 +1,4 @@
+function printInfo(name, age){
+    console.log(`${name}'s age is ${age}.`);
+}
+printInfo("Alina", 18);
