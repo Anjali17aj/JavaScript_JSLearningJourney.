afe67ef943606f743ed930269500044dcd519906
Anjali Kumari <sayhellotoanjali@gmail.com>
2023-09-03 15:12:37 +0530

    Array Methods - forEach

---
+++ Day11/foreach2.js
@@ -0,0 +1,6 @@
+
+let arr = [1,2,3,4,5,6,7];
+let print = function(el){
+    console.log(el);
+};
+arr.forEach(print);
---
+++ Day11/foreacharrw.js
@@ -0,0 +1,5 @@
+//foreach arrow function
+let arr = [1,2,3,4,5,6];
+arr.forEach((el) => {
+    console.log(el);
+});
