13d40878cffc62eac2a665b739044b6c6e4af333
Anjali Kumari <sayhellotoanjali@gmail.com>
2023-09-03 08:19:14 +0530

    Delete trycatch.js

--- trycatch.js
+++
@@ -1,8 +0,0 @@
-console.log("What a beautiful day");
-try {
-    console.log(a);
-} catch(err) {
-      console.log("Caught an error.... a is not defined");
-      console.log(err);
-}
-console.log("I play outside.");
