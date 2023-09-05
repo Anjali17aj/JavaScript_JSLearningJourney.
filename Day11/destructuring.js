18a641131a65d71c5dafb42c218291ec319ea625
Anjali Kumari <sayhellotoanjali@gmail.com>
2023-09-05 08:25:15 +0530

    Destructuring

---
+++ Day11/destructuring.js
@@ -0,0 +1,11 @@
+//destructuring
+
+let names = ["tony","peter","Steve","captain","chiky"];
+let winner = names[0];
+let runnerup = names[1];
+let secondRunnerup = names[2];
+
+              //or
+//let [ winner, runnerup, secondRunnerup, other] = names;
+               //or
+//let [ winner, runnerup, secondRunnerup, ...other] = names;
