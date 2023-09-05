35bc9b26c663be678d151c0f588ac01813817321
Anjali Kumari <sayhellotoanjali@gmail.com>
2023-09-05 06:35:25 +0530

    Spread (Object Literals)

---
+++ Day11/SpreadObjLit2.js
@@ -0,0 +1,5 @@
+//using spread with object literal
+//array has only values
+//but objects has key and value 
+let arr = [1,2,3,4];
+let obj1 = console.log({...arr});  //index and value both are printed.
---
+++ Day11/spreadObjLiterals.js
@@ -0,0 +1,9 @@
+
+//-------object---------key and value
+const data = {
+    email: "sayhellotoanjali@gmail.com",   
+    password: "abcd",
+
+};
+
+const dataCopy = console.log({ ...data, id: 123});
