3c0f3495c53779568353547d94256d762a45573a
Anjali Kumari <sayhellotoanjali@gmail.com>
2023-09-10 17:45:11 +0530

    DOM Manipulations. (JavaScript+Console)

---
+++ Day12/DomManipulation.js
@@ -0,0 +1,7 @@
+//Notes DOM Manipulation.
+
+//> Go to console 
+console.dir(document);
+console,dir(document.all);
+console,dir(document.all[8].innerText = "PeterSon");
+
---
+++ Day12/selectingElementsDom.js
@@ -0,0 +1,17 @@
+//1) 
+document.getElementById(id);
+
+let imgObj = document;
+imgObj
+console.log(imgObj);
+console.log(imgObj);
+imgObj.scr
+imgObj.scr = "assets/anjali_png";
+imgObj.scr.tagName
+imgObj.scr.id
+
+//2)
+document.getElementsByClassName("oldName");
+
+//3)
+document.getElementsByTagName("p");
