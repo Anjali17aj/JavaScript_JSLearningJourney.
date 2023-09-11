caf00d4f3f491e1026a3b401c8061cf96e27e5bc
Anjali Kumari <sayhellotoanjali@gmail.com>
2023-09-11 12:50:37 +0530

    DOM Manipulations. (JavaScript+Console)

---
+++ Day12/QuerySelectors.js
@@ -0,0 +1,12 @@
+//notes on query selector
+
+console.dir(document.querySelector("p"));
+console.dir(document.querySelector("#id"));
+console.dir(document.querySelector(".myClass"));
+console.dir(document.querySelector("div a"));
+
+console.dir(document.querySelectorAll("div a"));
+
+console.dir(document.querySelectorAll("p"));
+
+
