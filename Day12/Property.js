2976d9df9e8b3a3aaedfe54ebde750cf684fd650
Anjali Kumari <sayhellotoanjali@gmail.com>
2023-09-11 12:50:09 +0530

    DOM Manipulations. (JavaScript+Console)

---
+++ Day12/Property.js
@@ -0,0 +1,12 @@
+//DOM Manipulation
+//innerText
+//textContent
+//innerHtml
+
+
+let para = document.querySelector("p");
+para
+console.dir(para);
+
+para.innerText = "Anjali is a good student";
+para.innerHTML = "Hi, I am <b>Anjali</b>"
